6cc9191e161bbe94e867a104ffe93cc0fbe53e3f
Ricardo Silva <ricardo.silva@magicbeans.pt>
2023-01-16 18:36:23 +0000

    wip

---
+++ list_total_ebs.py
@@ -0,0 +1,18 @@
+import boto3
+
+ec2_ebs = boto3.client('ec2')
+
+volumes_available_total_gb = 0
+volumes_used_total_gb = 0
+
+#List all Available volumes (Not attached)
+for vol in ec2_ebs.describe_volumes()['Volumes']:
+    if vol['State'] == 'available':
+        volumes_available_total_gb += vol['Size']
+    else:
+        volumes_used_total_gb += vol['Size']
+
+total_size = volumes_available_total_gb + volumes_used_total_gb
+print ("Available: ", volumes_available_total_gb)
+print ("Attached: ", volumes_used_total_gb)
+print ("Total: ", total_size)
